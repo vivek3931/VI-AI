343dce6343a149b087e85b119f781da2722b262e
Vivek Singh <viveks3931@gmail.com>
2025-04-03 20:38:17 +0530

    first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,105 @@
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>VS AI</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            {" "}
+            <div className="greet">
+              <p>
+                <span>Hello, Duck.</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>What is the weather today?</p>
+                <img src={assets.question_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Summarize the following concept in easy words</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readability of the code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="user_icon" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src="src\assets\favicon-32x32.png" alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              value={input}
+              type="text"
+              placeholder="Enter here to search"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+             {input? <img
+                onClick={() => {
+                  onSent();
+                }}
+                src={assets.send_icon}
+                alt=""
+              />:null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            VS AI may display inaccurate info, including about people, so
+            double-check its responses. Your privacy is our concern
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+const Sidebar = () => {
+  const [extend, setExtend] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          src={assets.menu_icon}
+          alt=""
+          onClick={() => setExtend((prev) => !prev)}
+        />
+        <div
+          onClick={() => {
+            newChat();
+          }}
+          className="new-chat"
+        >
+          <img src={assets.plus_icon} alt="" />
+          {extend ? <p>New Chat</p> : null}
+        </div>
+        {extend ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {prevPrompts.map((item, idx) => {
+              return (
+                <div className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)}</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extend ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extend ? <p>Activity</p> : null}
+        </div>
+
+        <div
+          onClick={() => {
+            loadPrompt(item);
+          }}
+          className="bottom-item recent-entry"
+        >
+          <img src={assets.setting_icon} alt="" />
+          {extend ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
